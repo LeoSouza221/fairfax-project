96dbbe27f4e9aca2773c16a6686258973a72761d
LeoSouza221 <leosouza221@gmail.com>
2024-06-18 10:50:01 -0300

    first commit

---
+++ src/components/template/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar color="primary" height="70" app :elevation="0">
+    <v-app-bar-title>Fairfax Hoteis</v-app-bar-title>
+
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="sass">
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-responsive>
+      <v-img class="mb-4" height="150" src="@/assets/logo.png" />
+
+      <div class="py-4" />
+
+      <v-row>
+        <v-col cols="12"> teste </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
