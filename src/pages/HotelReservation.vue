5b72d5214f947381abd516398335d1e0aa9ce0c4
LeoSouza221 <leosouza221@gmail.com>
2024-06-19 17:14:27 -0300

    feat: create initial version of hotel reservation page

--- src/pages/HotelReservation.vue
+++ src/pages/HotelReservation.vue
@@ -1,9 +1,59 @@
 <template>
   <v-container>
-    <v-card></v-card>
+    <v-card
+      color="primary"
+      rounded="lg"
+      variant="outlined"
+    >
+      <v-card-text>
+        <form>
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model="userForm.name"
+                variant="outlined"
+                color="primary"
+                density="compact"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="userForm.email"
+                variant="outlined"
+                color="primary"
+                density="compact"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="userForm.phone"
+                variant="outlined"
+                color="primary"
+                density="compact"
+              ></v-text-field>
+            </v-col>
+            <v-col>
+              <v-select
+                v-model="userForm.payment"
+                :items="paymentMethod"
+                variant="outlined"
+                color="primary"
+                density="compact"
+              ></v-select>
+            </v-col>
+          </v-row>
+        </form>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
 <script setup lang="ts">
-//
+const paymentMethod = ['PIX', 'Boleto', 'Cartão crédito'];
+const userForm = ref({
+  name: '',
+  phone: '',
+  email: '',
+  payment: '',
+});
 </script>
